ee1a7fee775f284bea66c6f96104ba6556b5378d
Justin Hammond <justin@dynam.ac>
2015-01-12 23:05:57 +0800

    build LoggerCpp into FL Sources

--- fl/LoggerCpp/Exception.h
+++ fl/LoggerCpp/Exception.h
@@ -29,38 +29,40 @@
 #pragma warning(disable:4290)
 #endif
 
+namespace fl {
 
-namespace Log {
+	namespace Log {
 
-/**
- * @brief   Encapsulation of an error message based on std::runtime_error.
- * @ingroup LoggerCpp
- */
-class Exception : public std::runtime_error {
-public:
-    /**
-     * @brief Encapsulation of an error message based on std::runtime_error.
-     *
-     * @param[in] aErrorMessage The string message describing the error
-     */
-    explicit Exception(const std::string& aErrorMessage) :
-        std::runtime_error(aErrorMessage)
-    {}
-};
+		/**
+		 * @brief   Encapsulation of an error message based on std::runtime_error.
+		 * @ingroup LoggerCpp
+		 */
+		class Exception : public std::runtime_error {
+			public:
+				/**
+				 * @brief Encapsulation of an error message based on std::runtime_error.
+				 *
+				 * @param[in] aErrorMessage The string message describing the error
+				 */
+				explicit Exception(const std::string& aErrorMessage) :
+				std::runtime_error(aErrorMessage)
+				{}
+		};
 
 
-/// @brief Stringify 1/2 : convert an integer to a string (using the following macro)
+		/// @brief Stringify 1/2 : convert an integer to a string (using the following macro)
 #define TOSTRING(x) _XSTRING(x)
-/// @brief Stringify 2/2 : convert an integer to a string (inner macro)
+		/// @brief Stringify 2/2 : convert an integer to a string (inner macro)
 #define _XSTRING(x) #x
 
 #ifdef __FUNCTION__
-/// @brief Define __func__ under Windows, to use the same name as with GCC
+		/// @brief Define __func__ under Windows, to use the same name as with GCC
 #define __func__    __FUNCTION__
 #endif
 
-/// @brief Helper macro to throw an Exception with file/line/function information, using the string stream Formatter
+		/// @brief Helper macro to throw an Exception with file/line/function information, using the string stream Formatter
 #define LOGGER_THROW(x) throw Exception(Formatter() << __FILE__ << ":" << TOSTRING(__LINE__) << ": " << __func__ << "(): " << x)
 
+	}
 
 }  // namespace Log
